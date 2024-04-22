ca5e0069ca379d852c719577d800b99310000227
szymon-kw <szymonkwiecien7@w.pl>
2024-04-23 00:40:01 +0200

    Initial commit

---
+++ src/test/java/pl/autobase/AutobaseApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.autobase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutobaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
