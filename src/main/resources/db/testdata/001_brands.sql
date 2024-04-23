6943eda8c1a8f2dd88482de9562e6045a374d20f
szymon-kw <szymonkwiecien7@w.pl>
2024-04-23 13:10:39 +0200

    Added test data

---
+++ src/main/resources/db/testdata/001_brands.sql
@@ -0,0 +1,10 @@
+insert into brand (name) values ('Toyota');
+insert into brand (name) values ('GMC');
+insert into brand (name) values ('Mitsubishi');
+insert into brand (name) values ('Nissan');
+insert into brand (name) values ('BMW');
+insert into brand (name) values ('Lotus');
+insert into brand (name) values ('Pontiac');
+insert into brand (name) values ('Chevrolet');
+insert into brand (name) values ('Kia');
+insert into brand (name) values ('Audi');
